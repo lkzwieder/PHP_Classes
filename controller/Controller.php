75f7fc2477d9ee9b4839c022fa80a28b8b10629d
Lucas Tettamanti <lucas.tettamanti@gmail.com>
2015-04-01 15:11:37 -0300

    controller class refactored to suit a general context

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,19 +1,30 @@
 <?php
 class Controller {
+  const PREFIX_LENGTH = 11;
+  const VIEWS_DIRECTORY = "Views";
+  const APP_ROOT = "/app/root/";
+
   private $vars = [];
   private $view;
+
   public function __construct() {
-    $this->set_view(substr(get_called_class(), 12)); # controllers
+//    if your class is called Controller_Something we cut the Controller_ and obtain the name of the default view in
+//    order to set the view, you can change this if you want in the next line.
+    $this->set_view(substr(get_called_class(), self::PREFIX_LENGTH));
   }
+
   public function set_vars(Array $args) {
     $this->vars = $args;
   }
+
   public function render($filename = false) {
     if($filename) $this->set_view($filename);
+//    $view is used in the template $this->view.'.php';
     $view = (object) $this->vars;
     require $this->view.'.php';
   }
+
   public function set_view($filename) {
-    $this->view = path(APP_ROOT, "Views", strtolower($filename));
+    $this->view = implode(DIRECTORY_SEPARATOR, [self::APP_ROOT, self::VIEWS_DIRECTORY, strtolower($filename)]);
   }
 }
